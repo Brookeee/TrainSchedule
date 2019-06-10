e1d034adcb072ec4eeaa0eeb48ceed85aa95acfb
Brookee <oliver.brooke.a@gmail.com>
2019-06-10 09:31:45 -0400

    js

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
     // Firebase 
     var firebaseConfig = {
         apiKey: "AIzaSyCpdi7JmX5YD141KC_3TMmuTjPf5RA-3UM",
@@ -12,9 +13,45 @@ $(document).ready(function(){
 
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
+
+      var database = firebase.database().ref();
+
+      // Add new train info 
+
+      $("#submit").on("click", function(event){
+        event.preventDefault();
+      })
+
+      var tName = $("#train-name").val().trim();
+      var destination = $("#godestination").val().trim();
+      var tTime = $("#time-train").val().trim();
+      var freq = $("#freqlevel").val().trim();
+
+      // new train data holder 
+      var newData = {
+        trainName: tName,
+        tDest: destination,
+        trainTime: tTime,
+        tFreq: freq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      };
+
+      database.ref().push(newData);
+
+      // Console log
+      console.log(newData.trainName);
+      console.log(newData.tDest);
+      console.log(newData.trainTime);
+      console.log(newData.tFreq);
+
+      // clear
+      $("#train-name").val("");
+      $("#godestination").val("");
+      $("#time-train").val("");
+      $("#freqlevel").val("");
+});
+// Event to add new train info to database
+database.ref().on("child_create", function(childSnapshot){
+
 })
 
-// Firebase connection
-
-// Capture user input values + (trim)
-// Variables for new train info(name,dest, time, freq)
